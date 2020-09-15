273a5e4c4329f3a80a4e7e9e310cf98cb3bb03ee
Deyvson da Silva <DSILVA@MMSI.COM>
2020-09-15 11:56:17 -0300

    Project initialization

--- src/vis_client_example.py
+++ src/vis_client_example.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 VIS_URL = "wss://wwwivi:8088/"
 
 # Go to https://webhook.site/#/ copy and paste server link to HTTP_REQUEST_RECEIVER_URL
-HTTP_REQUEST_RECEIVER_URL = "<replace with your url>"
+HTTP_REQUEST_RECEIVER_URL = "https://e103c97d2cc01d14bdc4a4e0049ac8b8.m.pipedream.net"
 
 DATA_SENDING_DELAY = 2
 WAIT_TIMEOUT = 5
